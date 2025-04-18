e8c81a00d2e0fe31ab6fb9cbca344589e5fba63b
theijhay <olawaleisaacjohn@gmail.com>
2025-04-18 05:23:04 +0100

    refactor: Update .env.example and package.json
    - Update .env.example to include database and JWT authentication configurations
    - Add typeorm commands to package.json for database management

---
+++ src/migrations/1744949994381-InitSchema.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitSchema1744949994381 implements MigrationInterface {
+    name = 'InitSchema1744949994381'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying NOT NULL, "password" character varying NOT NULL, "is_active" boolean NOT NULL DEFAULT true, "is_verified" boolean NOT NULL DEFAULT false, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "collections" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "tableName" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" uuid, CONSTRAINT "UQ_73411c82d4c6e2c01405923489e" UNIQUE ("tableName"), CONSTRAINT "PK_21c00b1ebbd41ba1354242c5c4e" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "fields" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "type" character varying NOT NULL, "collectionId" uuid, CONSTRAINT "PK_ee7a215c6cd77a59e2cb3b59d41" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "collections" ADD CONSTRAINT "FK_da613d6625365707f8df0f65d81" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "fields" ADD CONSTRAINT "FK_a55ba1a49f7f5a59b3807f5d86f" FOREIGN KEY ("collectionId") REFERENCES "collections"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "fields" DROP CONSTRAINT "FK_a55ba1a49f7f5a59b3807f5d86f"`);
+        await queryRunner.query(`ALTER TABLE "collections" DROP CONSTRAINT "FK_da613d6625365707f8df0f65d81"`);
+        await queryRunner.query(`DROP TABLE "fields"`);
+        await queryRunner.query(`DROP TABLE "collections"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+    }
+
+}
---
+++ src/ormconfig.ts
@@ -0,0 +1,24 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import { parse } from 'pg-connection-string';
+
+import { User } from './users/users.entity';
+import { Collection } from './collections/collection.entity';
+import { Field } from './fields/field.entity';
+
+dotenv.config();
+const parsed = parse(process.env.DATABASE_URL!);
+
+export default new DataSource({
+  type: 'postgres',
+  host: parsed.host,
+  port: parseInt(parsed.port || '5432'),
+  username: parsed.user,
+  password: parsed.password,
+  database: parsed.database,
+
+  synchronize: false,
+  entities: [User, Collection, Field],
+  migrations: ['src/migrations/**/*{.ts,.js}'],
+  migrationsTableName: 'migrations',
+});
